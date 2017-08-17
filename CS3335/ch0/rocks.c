24d619bb6fdd68d04d1753e44a9bc671c68a4f70
Joanne Wardell <jawardell@valdosta.edu>
2017-08-17 16:35:00 -0400

    notes

--- CS3335/ch0/rocks.c
+++ CS3335/ch0/rocks.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <limits.h>
-
+#include <float.h> //this contains the e constant for scientific notation
 int main () {
 	puts("C Rocks!");
 	printf("CHAR_MIN=%d\n", CHAR_MIN);
+	printf("FLT_MAX=%.10e\n", FLT_MAX);
 	return 0;
 }
 
