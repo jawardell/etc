2d2a58646bb3e1954663dd10daf8a14258558ed2
Joanne Wardell <jawardell@valdosta.edu>
2017-08-22 16:37:23 -0400

    notes

--- CS3335/ch0/eof.c
+++ CS3335/ch0/eof.c
@@ -3,10 +3,26 @@
 
 int main(){
 	// Won't work
-	// unsigned char c;
+	 unsigned char c;
+
+
+
+//why?? 
+
+//input is read in 
+//type that varible as an int or signed char
+//DO NOT type is as unsigned char because control d will NOT allow the program to exit!!!!
+
+//unsigned chars are not automatically promoted to int type!!!!!!!
+
+
+
+//sizeof(EOF)   , make this function your friend!!! bffls <3 [sizeof()]
+
+
 
 	// Will work. signed char, char will do too.
-	int c;
+	//int c;
 
 	// To find out if char is signed or unsigned by default
 	printf ("CHAR_MIN=%d\n", CHAR_MIN);
