e50a3af14e10eca8f989824f227c67e9d43ae6ef
Joanne Wardell <jawardell@valdosta.edu>
2017-09-14 16:50:00 -0400

    more notes

--- CS3335/ch1/cookie.c
+++ CS3335/ch1/cookie.c
@@ -8,7 +8,7 @@ void fortune(char *msg){
 }
 
 int main(void){
-	char quote[]="fat";
+	char quote[]="Cookies are good";
 	printf("\nsizeof(quote) = %d\n", sizeof(quote));
 	fortune(quote);// pass quote into the function as a pointer .. char
 		//pointer decay -- char *msg is the aise of the pointer space, 	size of char
