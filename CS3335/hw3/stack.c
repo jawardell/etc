b899cc3a9822259aff2be942caed027a6e649adc
Joanne Wardell <jawardell@valdosta.edu>
2017-09-14 15:24:14 -0400

    first implementation attempt at push

--- CS3335/hw3/stack.c
+++ CS3335/hw3/stack.c
@@ -5,6 +5,12 @@
 #define STACK_SIZE 10
 #define STACK_EMPTY -1
 
+
+
+
+
+
+
 void push(char [], // input/ouput - the stack
 	  char,	   // input - data being pushed onto the stack
 	  int *,   // input/output - pointer to the ***index*** of the top of stack
@@ -19,9 +25,75 @@ void push(char stack[],
 	  int *top,
 	  int max_size){
 
+
+
+
+/*
+
+stack[]    -- this is the rectangle holding our boxes of chars
+
+item       -- this is the character that we are pushing on to the stack
+
+*top       -- this is a pointer 
+		it has an address
+		it has a value
+			the address ~ where top is 
+			the value ~ non-sense memory address 
+		use the value of top to get to the current top of the stack
+
+
+		&top   -- where top is
+		*top   -- use the value of top 
+				as a memory address
+		top    -- the value stored in top
+				the memory address of
+				the last item in the stack
+		why do we need a pointer for this? 
+
+		who knows. 
+
+max_size     -- this is the maximum number of elements that the stack can hold 
+*/
+
+
+
+
+	if(*top == max_size - 1) {
+		return;
+	}
+	if((*top < max_size) && (*top != max_size - 1)) {
+		stack[*top + 1] = item;
+		*top++;
+		return;
+
+	}
+	if((*top < max_size) && (*top == max_size - 1)) {
+		stack[*top] = item;
+		return;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 	// Your code here ...
 	// Push item to the top of stack if it's not full. 
 	// Update the top pointer s/t it points to the index of the new top element on the stack.
+	
+	
+	
+
+	
+
+
 }
 
 char pop(char stack[],
