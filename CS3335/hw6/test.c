62c691e85aeb9fb9225fe2aa0049c4deacfd025b
Joanne A Wardell <jawardell@mathcsprod1.valdosta.edu>
2017-10-09 23:32:11 -0400

    trying a new approach

---
+++ CS3335/hw6/test.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char* arguments;
+int b = 0, c = 0, d = 0, f = 0, n = 0, s = 0, i;
+char* file_name;
+
+int main(int argc, char* argv[]) {
+	for(i = 1; i < argc; i++) {
+		arguments = strcat()
+
+	}
+
+	
+	return 0;
+}
