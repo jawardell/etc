b4eaa8193ea6c5cd0a95df81dbccd0d6fddcd683
Joanne A Wardell <jawardell@mathcsprod1.valdosta.edu>
2017-10-10 17:05:41 -0400

    i hate u sprintf

--- CS3335/hw6/test.c
+++ CS3335/hw6/test.c
@@ -9,7 +9,7 @@ int b = 0, c = 0, d = 0, f = 0, n = 0, s = 0, i;
 char* file_name;
 
 int main(int argc, char* argv[]) {
-	puts("ok we are here");
+	printf("\nargc is %d\n", argc);
 	for(i = 1; i < argc; i++) {
 		printf("\niteration %d argv[%d] is : %s\n", i, i, argv[i]);
 		if(strstr(argv[i], "b")) {
@@ -48,17 +48,59 @@ int main(int argc, char* argv[]) {
 			sstring = argv[i];
 			printf("\nsstring is %s\n", sstring);
 		}
+		//this won't work because the program doesn't see 
+		//input redirection in the argv or argc info
+		//	.. i don't think this part is neccesary though.. 
+		//i just have to use the system function and pass the 
+		//final string concatenation as the argument. . . 
 		if(strstr(argv[i], "<")) {
 			puts("look where we are! we have a < input redirection");
 			puts("proceeding with next character check.. what is the index?");
 			printf("\nthe current index is %f, going to check index %f\n", i, i+1);
-			if(i != argc) {
+			if(i != (argc-1)) {
 				puts("going to assign filename = argv[i+1]..");
 				file_name = argv[i+1];
 				printf("\nfile_name is %s\n", file_name);
 			}
 		}
 
+	}
+	if(b){
+		puts("\nconcatenating bstring and arguments\n");
+		char* temp = arguments;
+		sprintf(arguments, "%s ", temp);
+		//arguments = strcat(bstring, "");
+		printf("\narguments is %s\n", arguments);
+	}
+	if(c){
+		puts("\nconcatenating cstring and arguments\n");
+		char* temp = arguments;
+		arguments = strcat(temp, cstring);
+		printf("\narguments is %s\n", arguments);
+	}
+	if(d){
+		puts("\nconcatenating dstring and arguments\n");
+		char* temp = arguments; 
+		arguments = strcat(temp, dstring);
+		printf("\narguments is %s\n", arguments);
+	}
+	if(f){
+		puts("\nconcatenating fstring and arguments\n");
+		char* temp = arguments;
+		arguments = strcat(temp, fstring);
+		printf("\narguments is %s\n", arguments);
+	}
+	if(n){
+		puts("\nconcatenating nstring and arguments\n");
+		char* temp = arguments;
+		arguments = strcat(temp, nstring);
+		printf("\narguments is %s\n", arguments);
+	}
+	if(s){
+		puts("\nconcatenating sstring and arguments\n");
+		char* temp = arguments;
+		arguments = strcat(temp, sstring);
+		printf("\narguments is %s\n", arguments);
 	}
 
 	
