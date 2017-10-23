94e1fbda0747c70cb980337088f47849f32886e7
Joanne A Wardell <jawardell@mathcsprod1.valdosta.edu>
2017-10-23 19:45:50 -0400

    board

--- CS3335/hw7/game.c
+++ CS3335/hw7/game.c
@@ -2,31 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+int main() {
+	int rows, cols, i, j;
+	puts("\nenter rows\n");
+	scanf("%d", &rows);
+	printf("\nrows is %d\n", rows);
+	puts("\nenter cols\n");
+	scanf("%d", &cols);
+	printf("\ncols is %d\n", cols);
 
 
+	char array[rows][cols];
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < cols; j++) {
+			array[i][j] = '*';
+		}
+	}
+
+	int rowlength;
+
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < cols; j++) {	
+			if(j == cols-1) {
+				printf("%c\n", array[i][j]);
+			} else {
+				printf("%c", array[i][j]);
+			}
+		}
+	}
 
 
-void makeBoard(int, int);
-void clearBoard();
-void scanBoard();
-void getHood(int, int);
+	puts("\n\n");
+	int x, y;
+	puts("\n\twhere would you like to place an x?\n");
+	puts("\nenter x-coord\n");
+	scanf("%d", &x);
+	printf("\nx is %d\n", x);
+	puts("\n\tenter y-coord\n");
+	scanf("%d", &y);
+	printf("\ny is %d\n", y);
+	printf("\nthe coordinates are (%d, %d)\n", x, y);
 
-void makeBoard(int rows, int cols) {
+	array[x][y] = 'x';
 
-}
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < cols; j++) {
+			if(j == cols-1) {
+				printf("%c\n", array[i][j]);
+			} else {
+				printf("%c", array[i][j]);
+			}
+		}
+	}
 
-void clearBoard() {
-
-}
-
-
-void scanBoard() {
-
-}
-
-
-
-void getHood(int row, int col) {
-
-
+	puts("\n\n");
 }
