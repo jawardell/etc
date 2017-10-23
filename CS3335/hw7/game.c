0eece06f3b04ca936ba19ec47f40a9c0dc6d3f11
Joanne A Wardell <jawardell@mathcsprod1.valdosta.edu>
2017-10-23 18:21:48 -0400

    game of life init

---
+++ CS3335/hw7/game.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+void makeBoard(int, int);
+void clearBoard();
+void scanBoard();
+void getHood(int, int);
+
+void makeBoard(int rows, int cols) {
+
+}
+
+void clearBoard() {
+
+}
+
+
+void scanBoard() {
+
+}
+
+
+
+void getHood(int row, int col) {
+
+
+}
