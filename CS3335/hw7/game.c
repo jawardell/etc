d8e4b2be27edd5e0a0933c2ac6bf85e62432615a
Joanne A Wardell <jawardell@mathcsprod1.valdosta.edu>
2017-10-24 00:22:24 -0400

    doing stuff

--- CS3335/hw7/game.c
+++ CS3335/hw7/game.c
@@ -31,29 +31,37 @@ int main() {
 		}
 	}
 
+	int choice;
 
-	puts("\n\n");
-	int x, y;
-	puts("\n\twhere would you like to place an x?\n");
-	puts("\nenter x-coord\n");
-	scanf("%d", &x);
-	printf("\nx is %d\n", x);
-	puts("\n\tenter y-coord\n");
-	scanf("%d", &y);
-	printf("\ny is %d\n", y);
-	printf("\nthe coordinates are (%d, %d)\n", x, y);
+	do {
+		puts("\n\n");
+		printf("\nyour choice was %d .. \n", choice);
+		puts("\n\n");
+		int x, y;
+		puts("\n\twhere would you like to place an x?\n");
+		puts("\nenter x-coord\n");
+		scanf("%d", &x);
+		printf("\nx is %d\n", x);
+		puts("\n\tenter y-coord\n");
+		scanf("%d", &y);
+		printf("\ny is %d\n", y);
+		printf("\nthe coordinates are (%d, %d)\n", x, y);
 
-	array[x][y] = 'x';
+		array[x][y] = 'x';
 
-	for(i = 0; i < rows; i++) {
-		for(j = 0; j < cols; j++) {
-			if(j == cols-1) {
-				printf("%c\n", array[i][j]);
-			} else {
-				printf("%c", array[i][j]);
+		for(i = 0; i < rows; i++) {
+			for(j = 0; j < cols; j++) {
+				if(j == cols-1) {
+					printf("%c\n", array[i][j]);
+				} else {
+					printf("%c", array[i][j]);
+				}
 			}
 		}
-	}
 
-	puts("\n\n");
+		puts("\n\n");
+		puts("\nwould you like to place an x?  [1 yes  |  0 no]\n");
+		scanf("%d", &choice);
+
+	} while(choice);
 }
