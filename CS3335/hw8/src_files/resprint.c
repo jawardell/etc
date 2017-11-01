f50e5a691480bb9ec5a0b56b0ae88c2d2653e3c2
Joanne Wardell <jawardell@valdosta.edu>
2017-11-01 19:56:11 -0400

    removed array print call and tarred u up

--- CS3335/hw8/src_files/resprint.c
+++ CS3335/hw8/src_files/resprint.c
@@ -6,15 +6,14 @@ void resprint(double list[8], double diffs[8], double mean, double sdev) {
 	for(i = 0; i < 8; i++) {
 		*(diffs + i) = *(list + i) - mean;
 	}
-	printf("\nThe mean is %f.\n", mean);
-	printf("The standard deviation is %f\n\n", sdev);
+	printf("\nThe mean is %.3f.\n", mean);
+	printf("The standard deviation is %.3f.\n\n", sdev);
 	puts("Table of diff between data items and mean\n");
 	puts("------------------------------------------");
 	printf("%-20s%-20s%-20s\n", "Index", "Item", "Diff");
 	for(i = 0; i < 8; i++) {
 		if(*(diffs + i) < 0) {
-
-			printf("%-20d%-20.3f%-19.3f\n", i, *(list + i), *(diffs + i));
+			printf("%-20d%-20.3f%-20.3f\n", i, *(list + i), *(diffs + i));
 		} else {
 			printf("%-20d%-20.3f%-20.3f\n", i, *(list + i), *(diffs + i));
 		}
