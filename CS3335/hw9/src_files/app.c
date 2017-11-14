95d014b086b61be5cd88ba3542db97c014d8e23d
Joanne Wardell <jawardell@valdosta.edu>
2017-11-14 15:31:08 -0500

    don'

--- CS3335/hw9/src_files/app.c
+++ CS3335/hw9/src_files/app.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+int empty = 1;
 typedef struct soldier_type {
 	char* name;
 	struct soldier_type* next;
@@ -12,34 +14,39 @@ typedef struct soldier_type {
 
 
 void* insert(void* cursor, void* name) {
-	soldier* s;
+
 	soldier* curse = (soldier*)cursor;
 	char* name1 = (char*)name;
+
+	soldier* s;
+
 	if((s = malloc(sizeof(soldier))) == NULL) {
 		puts("no more room on heap");
 		puts("exiting now. . .");
 		return NULL;
 	}
 
+
 	//make room on heap for the name
 	//then put it on the heap
 	//then make the pointer in struct point to it
 	s -> name =  strdup(name1);
-	fprintf(stdout, "\nname is %s\n", s -> name);
+
+
 
 	//if list empty, there is no cursor
 	//make s point to itself
 	//then return s as the new cursor
-	if (curse == NULL) {
+	if(empty) {
 		s -> next = s;
 		return s;
 	}
-
+	
 	//otherwise, insert directly after the cursor
 	//and return s as the new cursor
+	s -> next = curse -> next;
 	curse -> next = s;
-	return s;	
-
+	return s;
 }
 
 void* release(void* cursor) {
@@ -55,42 +62,54 @@ void* release(void* cursor) {
 	return curse;
 }
 
-void* advance(void* cursor) {
+void* advance(void* cursor) {	
 	soldier* curse = (soldier*)cursor;
-	soldier* temp = curse -> next;
-	curse -> next = temp -> next;
-	return curse;
+	soldier* temp = curse;
+	curse = curse -> next;
+	soldier* current = curse -> next;
+	while((current -> next) != temp) {
+		current = current -> next;
+	}
+
+
+
+	return curse -> next;
 }
 
 void print(void* cursor) {
 	soldier* curse = (soldier*)cursor;
-	soldier* temp = curse -> next;
-	fprintf(stdout, "\n%s\n", temp -> name);
+	fprintf(stdout, "\n%s\n", curse -> name);
 }
 
 
 int main() {
-
-	soldier* cursor; 
-
-
-	char name[100];
-	while(fgets(name, sizeof(name), stdin) != NULL) { 
-
-		cursor = insert(&cursor, name);
-		fprintf(stdout, "\ninserted soldier %s ...\n", cursor -> name);
+	empty = 1;
+	soldier* cursor;
+	//cursor -> next = NULL;
+	double count = 0;
+	char input[100];
+	while(fgets(input, sizeof(input), stdin) != NULL) {	
+		input[strlen(input)-1] = '\0';	
+		count = count + 1;
+		cursor = insert(&cursor, input);
+		fprintf(stdout, "\ninserted soldier %s \nmem address:%p\n", cursor -> name, cursor);
+		empty = 0;
+	}
+	int i = 1;
+	puts("printing soldiers");
+	for(i = 0; i < count; i++) {
+		fprintf(stdout, "\nsoldier %d: %s \n", i, cursor -> name);
+		cursor = advance(&cursor);	
 	}
 
+
 	//kill every seventh one
-	int i = 1;
 
 	while((cursor -> next) != NULL) {
-		soldier* c = cursor -> next;
-		
 		if(i % 7 == 0) {
-			fprintf(stdout, "\n%s died\n", c -> name);
+			fprintf(stdout, "\n%s died\n", cursor -> name);
 		}
-		advance(&cursor);
+		cursor = release(&cursor);
 		i++;
 	}
 	fprintf(stdout, "\n%s survived\n", cursor -> name);
