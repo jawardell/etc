4354c2d1ee1e102c3b224de8328781ea29d72bd0
Joanne Wardell <jawardell@valdosta.edu>
2017-11-16 15:14:36 -0500

    getting ready for card sorting

---
+++ CS3335/hw10/src_files/app.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+define DECK_SIZE = 52
+
+
+typedef struct {
+	const char *face;
+	const char *suit;
+} card;
+
+
+void make_deck(card* const deck, card* face[], card* suit[]);
+void shuffle_deck(card* const deck);
+void deal_deck(const card* const);
+void compare(const void*, const void*);
+void qsort(void* base, size_t nmemb, size_t size, 
+	int(*compare)(const void*, const void*));
+
+
+
+int main() {}
