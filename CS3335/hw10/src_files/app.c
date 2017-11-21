ee2777174059d46335b1fb8fdf7a1f53d5367e16
Joanne Wardell <jawardell@valdosta.edu>
2017-11-21 11:10:52 -0500

    tarred up, took out comments

--- CS3335/hw10/src_files/app.c
+++ CS3335/hw10/src_files/app.c
@@ -82,12 +82,3 @@ int main() {
 
 
 }
-/*
-int compare(const void* card_a, const void* card_b) {
-	if((*(card*)card_a).facevalue == (*(card*)card_b).facevalue) {
-		return (*(card*)card_a).suitvalue - (*(card*)card_b).suitvalue;
-	}
-	return (*(card*)card_a).facevalue - (*(card*)card_b).facevalue;
-
-}
-*/
