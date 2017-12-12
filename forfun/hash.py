e51b806ceb2f372ef1b8050779372ca4a5ff8172
Joanne Wardell <jawardell@valdosta.edu>
2017-12-11 21:20:44 -0500

    cool thread

--- forfun/hash.py
+++ forfun/hash.py
@@ -1,5 +1,12 @@
 import hashlib
+import os
+import time
 def hash(string):
 	hash_object = hashlib.md5(string.encode())
 	print(hash_object.hexdigest())
-hash("test")
+
+
+string = raw_input()
+hash(string)
+time.sleep(3)
+os.system('clear')
