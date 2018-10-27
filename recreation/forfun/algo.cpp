c1fae124412e23dec85d3e6efb9109e08ce007da
Joanne Wardell <jawardell@valdosta.edu>
2018-10-27 10:09:54 -0400

    updtea

--- recreation/forfun/algo.cpp
+++ recreation/forfun/algo.cpp
@@ -1,84 +1,96 @@
 #include <iostream>
+//#include "sha256.h"
+#include <map>
+#include <string>
 #include <fstream>
 #include <cmath>
+
 using namespace std;
- 
+
 struct Element{
-  int ID;
-  int count;
+	string ID;
+	int count;
 };
- 
+
 int main()
 {
-  float phi, epsilon;
-  cout << "Enter phi: ";
-  cin >> phi;
-  cout << "Enter epsilon: ";
-  cin >> epsilon;
- 
-  Element items[(int)ceil(1/epsilon)];
-  int len = ceil(1/epsilon);
- 
-  for(int i = 0; i < len; i++){
-	items[i].ID = 0;
-	items[i].count = 0;
-  }
- 
-  int n;
-  int m = 0;
- 
-  ifstream ifs;
-  ifs.open("/Users/joannewardell/data.txt");
-  ifs >> n;
-  cout << n << endl;
-  while(!ifs.eof()) {
-	m++;
- 
-	bool found = false;
+	float phi, epsilon;
+	cout << "Enter phi: ";
+	cin >> phi;
+	cout << "Enter epsilon: ";
+	cin >> epsilon;
+
+	Element items[(int)ceil(1/epsilon)];
+	int len = ceil(1/epsilon);
+
+	//initialize array to empty key value pairs
 	for(int i = 0; i < len; i++){
-  	if(items[i].ID == n){
-    	items[i].count++;
-    	found = true;
-    	break;
-  	}
+		items[i].ID = "";
+		items[i].count = 0;
 	}
- 
-	bool full = true;
-	int index;
-	if(!found){
-  	for(int i = 0; i < len; i++){
-    	if(items[i].count == 0){
-      	full = false;
-      	index = i;
-    	}
-  	}
- 
-  	if(!full){
-    	items[index].ID = n;
-    	items[index].count = 1;
-  	}
-  	else{
-    	for(int i = 0; i < len; i++)
-          if(items[i].count > 0)
-            items[i].count--;
-  	}
-	}
- 
+
+
+	map<int, string> addresses;
+
+
+	string n;
+	int m = 0;
+	string path = "/Users/joannewardell/Desktop/joanne/code/misc/etc/recreation/forfun/synthetic.txt";
+
+	ifstream ifs;
+	ifs.open(path);
 	ifs >> n;
-  }
- 
-  cout << "\nNumbers:\n";
-  int low = (phi - epsilon) * m;
-  cout << "low: " << low << endl;
-  for(int i = 0; i < len; i++)
-	if(items[i].count >= low)
-  	cout << items[i].ID << endl;
- 
-  cout << "\nArray:\n";
-  for(int i = 0; i < len; i++)
-	cout << items[i].ID << "   " << items[i].count << endl;
- 
-  ifs.close();
-  return 0;
+	cout << n << endl;
+	while(!ifs.eof()) {
+		m++;
+		//map.insert()
+		bool found = false;
+		for(int i = 0; i < len; i++){
+			if(items[i].ID.compare(n) == 0){
+				items[i].count++;
+				found = true;
+				break;
+			}
+		}
+
+		bool full = true;
+		int index;
+		if(!found){
+			for(int i = 0; i < len; i++){
+				if(items[i].count == 0){
+					full = false;
+					index = i;
+				}
+			}
+
+			if(!full){
+				items[index].ID = n;
+				items[index].count = 1;
+			}
+			else{
+				for(int i = 0; i < len; i++)
+					if(items[i].count >= 0)
+						items[i].count--;
+			}
+		}
+
+		ifs >> n;
+	}
+
+	cout << "\nNumbers:\n";
+	int low = (phi - epsilon) * m;
+	cout << "low: " << low << endl;
+	puts("heavy hitters\n");
+	for(int i = 0; i < len; i++)
+		if(items[i].count >= low)
+			cout << items[i].ID << "\t\t" << items[i].count << endl;
+		//	cout << items[i].ID << endl;
+
+	cout << "\nArray:\n";
+	//for(int i = 0; i < len; i++) 
+	//	cout << items[i].ID << "   " << items[i].count << endl;
+
+	ifs.close();
+	return 0;
 }
 
