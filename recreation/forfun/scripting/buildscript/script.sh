c35c86c025f3ac668caf713f3c65591db18c0def
Joanne <joanne.wardell6@gmail.com>
2019-05-22 14:53:17 -0400

    update

--- recreation/forfun/scripting/buildscript/script.sh
+++ recreation/forfun/scripting/buildscript/script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#my response to an exercise from codecademy.com
+
 #reset environment
 echo cleaning build folder
 cd build && rm * && ls && cd ..
@@ -10,6 +12,7 @@ echo 'Hello Human!'
 #extract the version number from changelog.md
 head -n 1 ./source/changelog.md > temp
 read firstline < temp
+rm temp
 #echo 1st line is: $firstline
 read -a splitfirstline <<< $firstline
 version=${splitfirstline[1]}
@@ -21,18 +24,18 @@ read versioncontinue
 regex='^[0-9]+'
 if ! [[ $versioncontinue =~ $regex ]]
 then
-	echo not a number
+  echo not a number
 elif [ $versioncontinue -eq 1 ]
 then
-	echo OK
-  
+  echo OK
+
   #copy every file from source to build
   files=source/*
   echo files: $files
   i=1
   for file in $files
   do
-  	#ignore secretinfo.md
+    #ignore secretinfo.md
     if ! [[ $file =~ 'secretinfo.md' ]]
     then
       cp $file ./build
@@ -40,50 +43,50 @@ then
     fi
     let 'i++'
   done
- 	
+
   #move into the build dir and list files
   cd build
   echo the contents of build $version are
   ls
-  
+
   #move out of the build dir when done
   cd ..
-  
+
 elif [ $versioncontinue -eq 0 ]
 then
-	echo please come back when ur ready
+  echo please come back when ur ready
 elif [ $versioncontinue -eq 2 ]
 then
-	echo youre using the robust script
-	echo it compresses the build dir
+  echo youre using the robust script
+  echo it compresses the build dir
   echo it uses emoji
   echo and git commits the changes at the end
-  
+
   #change 42 to XX in secretinfo.md
   #cp ./source/* ./build
   cd source && cp * ../build && cd .. 
   echo psuedo-encrypt secretinfo.md
   sed 's/42/XX/g' build/secretinfo.md
 
-  
+
   #compress the build directory
   echo compressing ./build
   tar cvf build.tar build
-  
-  
+
+
   #echo an emoji to the terminal
   # declare an array of emoji
-	EMOJIS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 🗿)
+  EMOJIS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 🗿)
 
-	# select a random element from the EMOJIS set
-	SELECTED_EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]};
+  # select a random element from the EMOJIS set
+  SELECTED_EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]};
   echo youre doing great ${SELECTED_EMOJI}
-  
 
 
-  
+
+
 else
-	echo bad input
+  echo bad input
 fi
 
 
