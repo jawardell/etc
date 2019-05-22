d8f33cd9ada515c7254260af8f1cd960f77b4a6c
Joanne <joanne.wardell6@gmail.com>
2019-05-22 14:40:14 -0400

    update

--- recreation/forfun/scripting/buildscript/script.sh
+++ recreation/forfun/scripting/buildscript/script.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-#This script extracts the version number from the changelog.md file
-#Asks the user for input about proceeding or not
+
+#reset environment
+echo cleaning build folder
+cd build && rm * && ls && cd ..
 
 echo 'Hello Human!'
-head -n 1 src/changelog.md > temp
+
+#extract the version number from changelog.md
+head -n 1 ./source/changelog.md > temp
 read firstline < temp
-echo 1st line is: $firstline
+#echo 1st line is: $firstline
 read -a splitfirstline <<< $firstline
 version=${splitfirstline[1]}
 echo version is: $version
-echo 'do you want to continue? (1 continue, 0 exit)'
+
+#prompt user to continue or not
+echo 'do you want to continue? (1 continue, 0 exit, 2 robust)'
 read versioncontinue
 regex='^[0-9]+'
 if ! [[ $versioncontinue =~ $regex ]]
@@ -18,9 +24,65 @@ then
 elif [ $versioncontinue -eq 1 ]
 then
 	echo OK
+  
+  #copy every file from source to build
+  files=source/*
+  echo files: $files
+  i=1
+  for file in $files
+  do
+  	#ignore secretinfo.md
+    if ! [[ $file =~ 'secretinfo.md' ]]
+    then
+      cp $file ./build
+      echo file $i: $file copied
+    fi
+    let 'i++'
+  done
+ 	
+  #move into the build dir and list files
+  cd build
+  echo the contents of build $version are
+  ls
+  
+  #move out of the build dir when done
+  cd ..
+  
 elif [ $versioncontinue -eq 0 ]
 then
 	echo please come back when ur ready
+elif [ $versioncontinue -eq 2 ]
+then
+	echo youre using the robust script
+	echo it compresses the build dir
+  echo it uses emoji
+  echo and git commits the changes at the end
+  
+  #change 42 to XX in secretinfo.md
+  #cp ./source/* ./build
+  cd source && cp * ../build && cd .. 
+  echo psuedo-encrypt secretinfo.md
+  sed 's/42/XX/g' build/secretinfo.md
+
+  
+  #compress the build directory
+  echo compressing ./build
+  tar cvf build.tar build
+  
+  
+  #echo an emoji to the terminal
+  # declare an array of emoji
+	EMOJIS=(😺 😸 😹 😻 😼 😽 🙀 😿 😾 🗿)
+
+	# select a random element from the EMOJIS set
+	SELECTED_EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]};
+  echo youre doing great ${SELECTED_EMOJI}
+  
+
+
+  
 else
 	echo bad input
 fi
+
+
