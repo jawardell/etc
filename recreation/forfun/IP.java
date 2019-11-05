010bcdbc9edd635b440ee20311ee9cca846d78ea
joanne wardell <jawardell@valdosta.edu>
2019-11-05 17:45:43 -0500

    update

--- recreation/forfun/IP.java
+++ recreation/forfun/IP.java
@@ -11,11 +11,15 @@ public class IP {
     Scanner scanner = new Scanner(System.in);
     int netmask = -1;
     if(args.length != 0) {
+      if(args[0].equals("pro")) {
+        proMode();
+      } 
       netmask = Integer.parseInt(args[0]); 
-    } else {
+    } else  {
       System.out.print("\n\n\tenter the netmask value 0-32\n");
       netmask = scanner.nextInt();
     }
+
     if(netmask < 0) {
       System.out.println("netmask was never initialized. exiting now.");
       System.exit(1);
@@ -34,11 +38,12 @@ public class IP {
     }
     int host = 32 - netmask;
     String prefix = getNetworkPrefix(netmask);
-    System.out.printf("\n\n\tthere are %d total ip addresses with a /%d netmask\n", (int)Math.pow(2,host), netmask);
+    System.out.printf("\n\n\ta /%d block can accommodate %d ip addresses\n", netmask, (int)Math.pow(2,host));
     for(int i = 0; i < Math.pow(2, host); i++) {
       String node = getBinary(i, host);
-      System.out.print(getIpAddress(prefix, node) + "\n");
-      pw.printf("%s,\n", getIpAddress(prefix, node));
+      String binIp = getIpAddress(prefix, node);
+      System.out.print(binIp + " = " + getDecIp(binIp)+ "\n");
+      //pw.printf("%s,\n", getIpAddress(prefix, node));
     }
 
     pw.close();
@@ -75,16 +80,76 @@ public class IP {
     String res = new String();
     String sum = prefix + host;
     int pos = 0;
-    for(int i = 0; i < sum.length() + 3; i++) {
-      if(pos >= sum.length()) {
+    for(int i = 1; i <= 35 + 3; i++) {
+      if(pos == sum.length()) {
         break;
       }
-      if((pos != 0) && (pos % 8 == 0)) {
+      if((i != 32) && (i % 9 == 0)) {
         res += ".";
       } else {
         res += sum.charAt(pos);
+        pos++;
       }
-      pos++;
+    }
+    return res;
+  }
+
+  public static String getDecIp(String binIp) {
+    String[] bytes = binIp.split("\\.");
+    String res = new String();
+    int i = 1;
+    for(String s: bytes) {
+      res += Integer.parseInt(s, 2);
+      if(i != 4) {
+        res += ".";
+      }
+      i++;
+    }
+    return res;
+  }
+
+
+  public static String getBinIp(String decIp) {
+    String[] decs = decIp.split("\\.");
+    String res = new String();
+    int i = 1;
+    for(String s : decs) {
+      if(i != 4) {
+        res += ".";
+      }
+      res += Integer.toBinaryString(Integer.parseInt(s));
+      i++;
+    }
+    return res;
+  }
+
+  public static void proMode() {
+    System.out.print("\n\t\tPRO!!!!!!!\n\n");
+    Scanner input = new Scanner(System.in);
+    System.out.println("enter the netmask");
+    int netmask  = input.nextInt();
+    int host = 32 - netmask;
+    String prefix = getNetworkPrefix(netmask);
+    System.out.println("here's the prefix- "+ prefix);
+    String binIp = getIpAddress(prefix, getBinary(0, host));
+    System.out.println("here's the ip address- " + binIp);
+    String startIp = initIp(binIp.split("\\."));
+    System.out.println("here's the start IP- " + startIp + " = " + getBinIp(startIp));
+    System.exit(0);
+  }
+
+  public static String initIp(String[] ipArray) {
+    Scanner input = new Scanner(System.in);
+    String res = new String();
+    int i = 1;
+    for(String s : ipArray) {
+      int low = 0;
+      int high = Integer.parseInt(s, 2);
+      System.out.printf("Choose octect %d. Can range from %d to %d.\n", i, low, high);
+      res += input.next();
+      if(i != 4) {
+        res += ".";
+      }
     }
     return res;
   }
